65266da28e041e5b0bd891bd5f92c8090af0fc58
Sahil Kumar <sahilkumar142002@gmail.com>
2026-05-15 18:22:17 +0530

    refactor: remove UserDashboard component and clean up routing in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "./components/ui/sonner"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NotFound from "./app/NotFound"
-import UserDashboard from "./app/UserDashboard"
 import Login from "./app/Login"
 import QaDashboard from "./app/QADashboard"
 
@@ -14,10 +13,8 @@ export function App() {
       <Toaster />
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Login />} /> */}
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<QaDashboard />} />
-          <Route path="/userdashboard" element={<UserDashboard />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
